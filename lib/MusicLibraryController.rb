da4cfce37a29148ba3d7ac34ad20ae7f9163cb1f
sarah <s.j.condon01@gmail.com>
2019-10-23 02:08:59 +0000

    Automatically backed up by Learn

--- lib/MusicLibraryController.rb
+++ lib/MusicLibraryController.rb
@@ -120,15 +120,6 @@ class MusicLibraryController
   #     puts "Playing #{song.name} by #{song.artist.name}"
   #   end
   # end
-    puts "Which song number would you like to play?"
-    song_names = self.song_array
-    user_input = gets.chomp.to_i
-    if user_input > 0 && user_input <= self.library.size
-      chosen_input = song_names[user_input - 1]
-      chosen_input = name_extractor(chosen_input)[1]
-      song = Song.find_by_name(chosen_input)
-      puts "Playing #{song.name} by #{song.artist.name}" unless song == nil
-    end
-  end
+  
 
 end
